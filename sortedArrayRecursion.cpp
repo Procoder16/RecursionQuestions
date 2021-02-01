bd4761113043c65c36087ec5e3445c1f81aa2796
Soumik Mukherjee <smsoumik1608@gmail.com>
2021-02-02 00:25:57 +0530

    Sorted array checking using recursion added

---
+++ sortedArrayRecursion.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+bool sorted(int arr[], int n){
+
+    if(n==0){
+        return true;
+    }
+
+    else{
+        return (arr[0]<arr[1] && sorted(arr+1, n-1));
+    }
+}
+
+int main(){
+
+    int n;
+    cout<<"ENTER THE SIZE OF THE ARRAY:";
+    cin>>n;
+
+    int arr[n];
+
+    cout<<"ENTER THE ELEMENTS OF THE ARRAY:\n";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    bool ans = sorted(arr, n);
+
+    if(ans){
+        cout<<"THE GIVEN ARRAY IS SORTED";
+    }
+    else{
+        cout<<"THE GIVEN ARRAY IS NOT SORTED";
+    }
+
+    return 0;
+}
