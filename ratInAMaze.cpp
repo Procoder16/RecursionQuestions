e5f2f836dc03bddeab3491287ecf400be2cef5c1
Procoder16 <smsoumik1608@gmail.com>
2021-04-17 12:26:04 +0530

    rat in a maze question corrected

--- ratInAMaze.cpp
+++ ratInAMaze.cpp
@@ -1,81 +1,75 @@
 #include<iostream>
-
 using namespace std;
 
-bool isSafe(int** maze, int x, int y, int n){
-
-    if(x<n && y<n && maze[x][y] == 1){
+bool isSafe(int** arr, int x, int y, int n){
+    if(x<n && y<n && arr[x][y]==1){
         return true;
     }
-
-    return false;    
+    return false;
 }
 
-bool ratInAMaze(int** maze, int x, int y, int n, int** path){
+bool ratInMaze(int** arr, int x, int y, int n, int** solArr){
 
-    if((x == (n-1)) && (y == (n-1))){
-        path[x][y] = 1;
+    if(x==n-1 && y==n-1){
+        solArr[x][y] = 1;
         return true;
     }
-    if(isSafe(maze,x,y,n)){
-        path[x][y] = 1;
 
-        if (ratInAMaze(maze, x+1, y, n, path)){
+    if(isSafe(arr, x, y, n)){
+        solArr[x][y] = 1;
+
+        if(ratInMaze(arr, x+1, y, n, solArr)){
             return true;
         }
-        if (ratInAMaze(maze, x, y+1, n, path))
-        {
+
+        if(ratInMaze(arr, x, y+1, n, solArr)){
             return true;
         }
         
-        path[x][y] = 0;
-        return false;        
+        solArr[x][y] = 0;  
+        return false;
     }
+
     return false;
 }
 
 int main(){
 
     int n;
-    cout<<"ENTER THE SIZE OF THE SQUARE MATRIX:";
+    cout<<"INPUT THE SIZE OF THE MAZE:";
     cin>>n;
 
-    int** maze = new int*[n];
+    int** arr = new int*[n];
     for(int i=0; i<n; i++){
-        maze[i] = new int[n];
+        arr[i] = new int[n];
     }
 
-    cout<<"ENTER THE MAZE:";
+    cout<<"ENTER THE MATRIX COORDINATES:"<<endl;
+
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            cin>>maze[i][j];
+           cin>>arr[i][j]; 
         }
     }
 
-    int** path = new int*[n];
+    int** solArr = new int*[n];
     for(int i=0; i<n; i++){
-        path[i] = new int[n];
+        solArr[i] = new int[n];
         for(int j=0; j<n; j++){
-            path[i][j] = 0;
+            solArr[i][j] = 0;
         }
     }
 
-    if(ratInAMaze(maze, 0, 0, n, path)){
+    if(ratInMaze(arr, 0, 0, n, solArr)){
+        cout<<"PATH TO BE FOLLOWED:"<<endl;
         for(int i=0; i<n; i++){
-            for(int j=0; i<n; j++){
-                cout<<path[i][j]<<" ";
+            for(int j=0; j<n; j++){
+            cout<<solArr[i][j]<<" "; 
             }
             cout<<endl;
         }
     }
 
     return 0;
-}
+} 
 
-/*
-1 0 1 0 1 
-1 1 1 1 1 
-0 1 0 1 0 
-1 0 0 1 1 
-1 1 1 0 1
-*/
